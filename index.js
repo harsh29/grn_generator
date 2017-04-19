f4c3409ecb1711461c577231e94df8898dc10bf5
Harsh Trivedi <htrivedi@elementumcom>
2017-04-18 21:37:17 -0700

    second commit-generating files

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-var path = require('path');
-var jsf = require('json-schema-faker');
-var refaker = require('refaker');
+  var path = require('path');
+  var jsf = require('json-schema-faker');
+  var refaker = require('refaker');
+  var faker = require('faker/locale/en');
+  fs = require('fs');
 
-var schemasDirPath = path.join(__dirname, 'schemas');
-var schemaPath = path.join(__dirname, 'schemas', 'GRN.json');
-var schema = require(schemaPath);
 
-refaker({
-  schema: schema,
-  fakeroot: 'http://example.com',
-  directory: schemasDirPath
-}, function(err, refs, schemas) {
-  if(err) {
-    throw err;
-  }
+  var schemasDirPath = path.join(__dirname, 'schemas');
+  var schemaPath = path.join(__dirname, 'schemas', 'GRN.json');
+  var schema = require(schemaPath);
+  var args = process.argv.slice(2);
 
-  console.log('refs:', JSON.stringify(refs, null, 2));
-  console.log('schemas:', JSON.stringify(schemas, null, 2));
-  
-  var sample = jsf(schemas, refs);
+  jsf.extend('faker', function() {
+      // just ignore the passed faker instance
+        var faker = require('faker/locale/en');
+        return faker;
+  });
 
-  console.log('---------');
-  console.log('---------');
+  refaker({
+            schema: schema,
+            fakeroot: 'http://example.com',
+            directory: schemasDirPath
+          }, function(err, refs, schemas) {
+                      if(err) {
+                                throw err;
+          }
+          
+          var numberOfFiles = 10;
+          if(Number.isInteger(parseInt(args[0]))){
+              numberOfFiles = args[0];
+          }
+          console.log("Generating "+numberOfFiles + "files");
+          for(var i = 0; i< numberOfFiles; i++){
+              jsf(schemas, refs).then(function(sample) {
+              
+                var dir = './output';
 
-  console.log('sample:', JSON.stringify(sample, null, 2));
-});
+                if (!fs.existsSync(dir)){
+                                          fs.mkdirSync(dir);
+                }
+                fs.writeFile("./output/"+faker.random.number({min:10000, max:100000})+".json", JSON.stringify(sample, null, 2), function(err) {
+                  if(err) {
+                          return console.log(err);
+                  }
+                  console.log("The file was saved!");
+                });
+              });
+          }
+  });
